91ac6d53568cc3097636fb6ab44700a847f7b0ca
myfavoritecolorisyou <eunseokleedev@gmail.com>
2025-08-14 15:47:35 +0900

    commit

--- ia-mini/Program.cs
+++ ia-mini/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Statiq.App;
 using Statiq.Web;
+using Statiq.Markdown;
+using Markdig;
 
-return await Bootstrapper
-  .Factory
-  .CreateWeb(args)
+await Bootstrapper
+  .Factory.CreateWeb(args)
+  .AddSetting(MarkdownKeys.MarkdownExtensions, MarkdownExtensions.UseAdvancedExtensions)
+  // 또는 특정 확장만:
+  //.AddSetting(MarkdownKeys.MarkdownExtensions, new[] { "PipeTables", "GridTables" })
   .RunAsync();
