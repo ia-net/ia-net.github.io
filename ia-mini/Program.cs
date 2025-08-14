4159e44a52b87974fbb73aa0c51d929f8bff4583
myfavoritecolorisyou <eunseokleedev@gmail.com>
2025-08-14 16:20:04 +0900

    update highlightjs

--- ia-mini/Program.cs
+++ ia-mini/Program.cs
@@ -3,11 +3,9 @@ using Statiq.Web;
 using Statiq.Markdown;
 
 await Bootstrapper.Factory.CreateWeb(args)
-  // 확장 추가
   .ModifyTemplate(
       MediaTypes.Markdown,
       x => ((RenderMarkdown)x).UseExtensions())
-  // 확장 개별 지정 필요 시
   // .ModifyTemplate(
   //     MediaTypes.Markdown,
   //     x => ((RenderMarkdown)x).UseExtensions("pipetables+gridtables"))
